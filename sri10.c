f5095f9cdcdef93e67124343b63e072bd106ab0c
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-02-02 16:39:46 +0530

    Create sri10.c

---
+++ sri10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int i,a,b,flag=0;
+printf("\n entyer the value of a and b")
+for(i=2;i<=a/2;i++)
+{
+if(a%i==0)
+{
+flag=1;
+break:
+}
+}
+if(flag==0)
+printf("\n%d",i);
+a++;
+}
+getch();
+}
