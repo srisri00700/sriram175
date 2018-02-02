fccaf2358737b42cd9b34c29249400017a233442
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-02-02 16:26:51 +0530

    sri7.c

---
+++ sri7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+int s;
+printf("enter the num=");
+scanf("%d",&s);
+{
+	printf("\nThe number %dis not prime:",s);
+}
+else
+{
+	printf("\nThe number %dis prime:",s)
+  }
