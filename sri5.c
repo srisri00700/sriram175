44d9331f46d0d46e988ab3e2f17d782c09054748
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-02-02 16:21:25 +0530

    sri5.c

---
+++ sri5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<math.h>
+ 
+long power (inta, intb);
+ 
+int main()
+{
+    int power=3, num=2;
+    long result;
+ 
+    printf("%d", a);
+    scanf("%d", &b);
+    printf("%d", a);
+    scanf("%d", &a);
+    result = power(num, pow);
+    printf("%d^%d is %ld", b, a, result);
+    return 0;
+{
+    if (a)
+    {
+        return (b * a(b ,a- 1));
+    }
+    return 1;
+}
