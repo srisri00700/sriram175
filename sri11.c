6c103853cd684d693392c2e4a4ce01ad76ce52d7
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-02-02 16:42:44 +0530

    sri11.c

---
+++ sri11.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{ 
+int digit,sum=0,num,result;
+printf("\n enter the number");
+scanf("%d",&num);
+result=num;
+while(num!=0)
+{
+digit=num%10;
+}
+if(result==sum)
+{ printf(" \n yes");
+}
+else
+{ printf("\n no");
+}
+getch();
+}
