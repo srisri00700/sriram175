aad2764e5f201ade63bb5f96e434b186e13d20ee
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-02-05 16:41:12 +0530

    sri22.C

---
+++ sri22.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int array[100],n,i,largest;
+printf("\n enter the numbers);
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&array[i]);
+}
+largest=array[0];
+for(i=0;i<n;i++)
+{
+if(largest<array[i])
+largest=array[i];
+}
+printf("\n largest number in the array=%d",largest);
+getch();
+}
