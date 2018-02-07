3f4100250446977d5c85609829432b3b3ac26277
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-02-07 16:24:25 +0530

    sri51.c

---
+++ sri51.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int array[100],i,largest,size,j,temp;
+printf("\n enter the size");
+for(i=0;i<size;i++)
+{
+scanf("%d",&array[i]);
+}
+{
+if(largest<array[i])
+{   largest=array[i];
+}
+printf("%d",largest);
+}
+getch();
+}
