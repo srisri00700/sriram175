2b0e2fbefb8bbca5ba404865cea48f294b82993e
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-02-07 16:39:04 +0530

    sri222.c

---
+++ sri222.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a[50],i,size;
+printf("\n enter the size:");
+scanf("%d",&size);
+for(i=0;i<size,i++)
+{
+sacnf("%d",&a[i]);
+}
+for(i=0;i<size;i++)
+{
+printf("%d,%d",array[i],i);
+}
+getch();
+}
