361df74a022c737de5c72ca2c428bd76d177a0c7
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-02-12 16:17:55 +0530

    sri236.c

---
+++ sri236.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char a[50];
+	int i,count=1;
+	gets(a);
+	
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i] == ' ')
+		count++;
+		
+		
+	}
+	printf("%d",count);
+	return 0;
+}
