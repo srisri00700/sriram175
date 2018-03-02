ec3ffa3e016beff8a0edc6dd825f8fee31d933fe
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-03-02 02:38:45 -0800

    sri000.c

---
+++ sri000.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[10];
+int i,k;
+scanf("%s",&s);
+printf("the string is %s",s);
+scanf("%d",&k);
+printf("the k value is%d",k);
+for(i=0;i<k;i++)
+{
+printf("%s",s);
+}
+getch()
+}
