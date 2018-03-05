bf000cf50ea631e7e0061bbca9f91f5c98ec064f
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-03-05 02:31:25 -0800

    siva97.c

---
+++ siva97.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i;
+clrscr();
+printf("enter the number");
+scanf("%d",&i);
+n={"1,2,3,4,5,6,7,8,9,0"};
+if(n=i)
+{
+    printf("yes");
+}
+else
+{
+    printf("no");
+}
+return 0;
+getch();
+}
