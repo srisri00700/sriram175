08cf163d0e4bf33752c35384ec57648023b4a44d
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-03-05 03:07:38 -0800

    sri988.c

---
+++ sri988.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a[40],i,n,large,small;
+printf("enter how many array elements");
+scanf("%d",&a[i]);
+for(i=0;i<n;i++)
+{
+if(a[i]>large)
+large=a[i];
+if(a[i]<small)
+small=a[i];
+}
+{
+printf("enter the largest element %d",n);
+printf("enter the smallest element %d",n);
+return 0;
+getch();
+}
+}
