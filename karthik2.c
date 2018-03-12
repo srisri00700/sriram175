f4378235db559f6e149f7052529054b8ba0431ce
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-03-12 03:20:55 -0700

    karthik2.c

---
+++ karthik2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,k,c;
+scanf("%d",&n);
+scanf("%d",&k);
+c=n*k;
+if(c/2==0)
+printf("%d",Even);
+else
+printf("%d",odd);
+return 0;
+getch();
+}
