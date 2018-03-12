8024fff75441fbf5d1afd884d0f8e397b3f257fc
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-03-12 03:24:15 -0700

    karthik3.c

---
+++ karthik3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a[50]="hello.123";
+int i;
+if(a[i]>='a' && a[i]<='z' || a[i]>='1' && a[i]<='9')
+printf("yes");
+else
+printf("No");
+return 0;
+getch();
+}
