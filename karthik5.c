ebfd47e749005e8180efb85ea128e9303a46d52a
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-03-12 03:31:49 -0700

    karthik5.c

---
+++ karthik5.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("\nEnter the two elements:");
+scanf("%d %d",&a,&b);
+a^=b;
+b^=a;
+a^=b;
+printf("\nThe swapped values are:%d %d",a,b);
+return 0;
+}
+
