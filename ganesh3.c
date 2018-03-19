641a29454cbc8afed562b381f18f2e1aa040485f
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-03-19 16:08:02 +0530

    ganesh3.c

---
+++ ganesh3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+scanf("%d",&a);
+scanf("%d",&b);
+c=a+b;
+if(c%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+return 0;
+getch();
+}
