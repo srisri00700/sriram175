5136dc0de5af1ecca5750a5690fddd8eda781000
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-03-19 15:51:49 +0530

    ganesh1.c

---
+++ ganesh1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{	char a[20];
+	int i;
+	scanf("%s",&a);
+		if((a[i]=='0')||(a[i]=='1'))
+		{
+			printf("yes");
+		}
+		else 
+		{
+			printf("no");
+		}
+	return 0;
+  gatch();
+}
