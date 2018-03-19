e7939d8e7fe728a788b486f4155a76d95e2cd843
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-03-19 15:55:48 +0530

    ganesh2.c

---
+++ ganesh2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char b2[50];
+    int i,c=0;
+    printf("enter the string :");
+    scanf("%[^n]s",b2);
+    for(i=0;s2[i]!='\0';i++){
+if(b2[i]==' ')
+        {
+            c++;
+        }
+
+}
+ printf("no of words are :%d",c);
+return 0;
+getch();
+}
