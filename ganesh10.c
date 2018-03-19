4e54fb4f0e014014a8904735e38470fcde771afd
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-03-19 16:50:29 +0530

    ganesh10.c

---
+++ ganesh10.c
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+int n, num, d, rev = 0;
+     cout << "Enter a positive number: ";
+     cin >> num;
+     n = num;
+     do
+     {
+         d= num % 10;
+         rev = (rev * 10) + d;
+         num = num / 10;
+     } while (num != 0);
+     if (n == rev)
+     {
+         cout << " The number is a palindrome";
+     }
+     else
+     {
+         cout << " The number is not a palindrome";
+}
+	return 0;
+  getch();
+}
