1c6efcbc64d4068570231e5f56673c308cab22a4
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-03-19 16:22:19 +0530

    ganesh6.c

---
+++ ganesh6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+while((n%10)!=0)
+{
+n++;
+}
+{
+printf("nearest number is %d",n);
+}
+return 0;
+getch();
+}
